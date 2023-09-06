4db01de508af0598077a2b5f982dd802022e819e
Your Name <meho.aliefendic@gmail.com>
2023-09-06 22:29:27 +0200

    Setup arhitecture

---
+++ demo-service/src/main/java/com/ba/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.ba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class DemoApplication  {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
